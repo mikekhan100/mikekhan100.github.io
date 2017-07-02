1bf2368edb3bb5a9271c15b3cfdd55e4b1b906d0
Michael Khan <mikekhan100@users.noreply.github.com>
2017-07-02 21:44:10 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function selectMeal() {
     var mealsArray = ['T-bone Steak', 'Curry', 'Pizza', 'Roast Dinner', 'Spaghetti Bolognese', 'Chicken Pasta', 'Stir Fry', 'Sausage and Mash'];
-    var ranNum = function (mealsArray.length) {
+    var ranNum = function () {
       return Math.floor(Math.random() * (mealsArray.length));
     }
     
