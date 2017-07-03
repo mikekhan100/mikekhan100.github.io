4699e0cb03832b1c5c8a6ec013821854f7e53056
Michael Khan <mikekhan100@users.noreply.github.com>
2017-07-03 13:56:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function selectMeal() {
     var mealsArray = ['T-bone Steak', 'Curry', 'Pizza', 'Roast Dinner', 'Spaghetti Bolognese', 'Chicken Pasta', 'Stir Fry', 'Sausage and Mash'];
-    var ranNum = function () {
-      return Math.floor(Math.random() * (8));
-    }
+    var ranNum = Math.floor(Math.random() * (8));
     
-   /* var text = document.getElementById("textArea");
-    text.textContent = mealsArray[ranNum];*/
-
+    
     var text = document.getElementById("textArea");
-    text.textContent = 2;
+    text.textContent = mealsArray[ranNum];
+
+
 }
