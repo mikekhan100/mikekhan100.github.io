90b8e3ec89dc78288a8eb69ab7b440574bcc6fbd
Michael Khan <mikekhan100@users.noreply.github.com>
2017-07-09 21:33:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function selectMeal() {
-    var mealsArray = ['T-bone Steak', 'Curry', 'Pizza', 'Roast Dinner', 'Spaghetti Bolognese', 'Chicken Pasta', 'Stir Fry', 'Sausage and Mash'];
+    var chickenArray = ['Curry', 'Roast Dinner', 'Chicken Pasta', 'Stir Fry', 'Chicken and Ham Pie'];
+    var beefArray = ['Steak and Chips', 'Curry', 'Roast Dinner', 'Stir Fry'];
+    var lambArray = ['Curry', 'Roast Dinner', 'Lamb Chops and Chips'];
+    var fishArray = ['Fish and Chips', 'Fish Pie'];
+    var minceArray = ['Spaghetti Bolognese', 'Meatballs'];
+    var sausageArray = ['Sausage and Mash', 'Sausage Casserole'];
+    var miscellaneousArray = ['Pizza'];
     var ranNum = Math.floor(Math.random() * (mealsArray.length));
     
     
