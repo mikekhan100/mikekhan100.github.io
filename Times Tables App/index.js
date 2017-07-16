0231f155fdc93060dd18d358725248ecfd0ab5f0
Michael Khan <mikekhan100@users.noreply.github.com>
2017-07-16 12:18:06 +0100

    Update index.js

--- Times Tables App/index.js
+++ Times Tables App/index.js
@@ -1,6 +1,6 @@
 function calculate() {
     
-   var currentValue = document.getElementById("multiple");
+   var currentValue = document.getElementById("multiple").value;
    
    if (currentValue === 2) {
         var answer = document.getElementById("answerArea");
