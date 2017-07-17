7a0c17bdeb97b0e5159084180512ec092646f500
Michael Khan <mikekhan100@users.noreply.github.com>
2017-07-17 15:38:53 +0100

    Update index.js

--- Times Tables App/index.js
+++ Times Tables App/index.js
@@ -1,9 +1,152 @@
-function calculate() {
+function calculate1() {
     
-   var currentValue = document.getElementById("multiple").value;
-   var answer = document.getElementById("answerArea");
+   var currentValue = document.getElementById("input1").value;
+   var answer = document.getElementById("answer1");
    
    if(currentValue == 2) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate2() {
+    
+   var currentValue = document.getElementById("input2").value;
+   var answer = document.getElementById("answer2");
+   
+   if(currentValue == 4) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate3() {
+    
+   var currentValue = document.getElementById("input3").value;
+   var answer = document.getElementById("answer3");
+   
+   if(currentValue == 6) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate4() {
+    
+   var currentValue = document.getElementById("input4").value;
+   var answer = document.getElementById("answer4");
+   
+   if(currentValue == 8) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate5() {
+    
+   var currentValue = document.getElementById("input5").value;
+   var answer = document.getElementById("answer5");
+   
+   if(currentValue == 10) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate6() {
+    
+   var currentValue = document.getElementById("input6").value;
+   var answer = document.getElementById("answer6");
+   
+   if(currentValue == 12) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate7() {
+    
+   var currentValue = document.getElementById("input7").value;
+   var answer = document.getElementById("answer7");
+   
+   if(currentValue == 14) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate8() {
+    
+   var currentValue = document.getElementById("input8").value;
+   var answer = document.getElementById("answer8");
+   
+   if(currentValue == 16) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate9() {
+    
+   var currentValue = document.getElementById("input9").value;
+   var answer = document.getElementById("answer9");
+   
+   if(currentValue == 18) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate10() {
+    
+   var currentValue = document.getElementById("input10").value;
+   var answer = document.getElementById("answer10");
+   
+   if(currentValue == 20) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate11() {
+    
+   var currentValue = document.getElementById("input11").value;
+   var answer = document.getElementById("answer11");
+   
+   if(currentValue == 22) {
+        answer.textContent = "Correct";
+   }
+   else {
+       answer.textContent = "Please try again";
+   }
+}
+
+function calculate12() {
+    
+   var currentValue = document.getElementById("input12").value;
+   var answer = document.getElementById("answer12");
+   
+   if(currentValue == 24) {
         answer.textContent = "Correct";
    }
    else {
