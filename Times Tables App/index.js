6575a31c55df4e13328a3619c06feb993bfa14ec
Michael Khan <mikekhan100@users.noreply.github.com>
2017-07-26 14:14:17 +0100

    Update index.js

--- Times Tables App/index.js
+++ Times Tables App/index.js
@@ -10,7 +10,7 @@ var timesBy1 = document.getElementById("input1").value;
 var answer1 = document.getElementById("answer1");
   if (timesBy1 == 2) {
     answer1.textContent = "Correct!";
-    answer1.style.color = "#00db71";
+    answer1.style.color = "#00e030";
     prize++;
   }
   else {
@@ -25,10 +25,12 @@ var timesBy2 = document.getElementById("input2").value;
 var answer2 = document.getElementById("answer2");
   if (timesBy2 == 4) {
     answer2.textContent = "Correct!";
+    answer2.style.color = "#00e030";
     prize++;
   }
   else {
     answer2.textContent = "Please try again";
+    answer2.style.color = "#f40430";
   }
 }
 times2();
@@ -38,10 +40,12 @@ var timesBy3 = document.getElementById("input3").value;
 var answer3 = document.getElementById("answer3");
   if (timesBy3 == 6) {
     answer3.textContent = "Correct!";
+    answer3.style.color = "#00e030";
     prize++;
   }
   else {
     answer3.textContent = "Please try again";
+    answer3.style.color = "#f40430";
   }
 }
 times3();
@@ -51,10 +55,12 @@ var timesBy4 = document.getElementById("input4").value;
 var answer4 = document.getElementById("answer4");
   if (timesBy4 == 8) {
     answer4.textContent = "Correct!";
+    answer4.style.color = "#00e030";
     prize++;
   }
   else {
     answer4.textContent = "Please try again";
+    answer4.style.color = "#f40430";
   }
 }
 times4();
@@ -64,10 +70,12 @@ var timesBy5 = document.getElementById("input5").value;
 var answer5 = document.getElementById("answer5");
   if (timesBy5 == 10) {
     answer5.textContent = "Correct!";
+    answer5.style.color = "#00e030";
     prize++;
   }
   else {
     answer5.textContent = "Please try again";
+    answer5.style.color = "#f40430";
   }
 }
 times5();
@@ -77,10 +85,12 @@ var timesBy6 = document.getElementById("input6").value;
 var answer6 = document.getElementById("answer6");
   if (timesBy6 == 12) {
     answer6.textContent = "Correct!";
+    answer6.style.color = "#00e030";
     prize++;
   }
   else {
     answer6.textContent = "Please try again";
+    answer6.style.color = "#f40430";
   }
 }
 times6();
@@ -90,10 +100,12 @@ var timesBy7 = document.getElementById("input7").value;
 var answer7 = document.getElementById("answer7");
   if (timesBy7 == 14) {
     answer7.textContent = "Correct!";
+    answer7.style.color = "#00e030";
     prize++;
   }
   else {
     answer7.textContent = "Please try again";
+    answer7.style.color = "#f40430";
   }
 }
 times7();
@@ -103,10 +115,12 @@ var timesBy8 = document.getElementById("input8").value;
 var answer8 = document.getElementById("answer8");
   if (timesBy8 == 16) {
     answer8.textContent = "Correct!";
+    answer8.style.color = "#00e030";
     prize++;
   }
   else {
     answer8.textContent = "Please try again";
+    answer8.style.color = "#f40430";
   }
 }
 times8();
@@ -116,10 +130,12 @@ var timesBy9 = document.getElementById("input9").value;
 var answer9 = document.getElementById("answer9");
   if (timesBy9 == 18) {
     answer9.textContent = "Correct!";
+    answer9.style.color = "#00e030";
     prize++;
   }
   else {
     answer9.textContent = "Please try again";
+    answer9.style.color = "#f40430";
   }
 }
 times9();
@@ -129,10 +145,12 @@ var timesBy10 = document.getElementById("input10").value;
 var answer10 = document.getElementById("answer10");
   if (timesBy10 == 20) {
     answer10.textContent = "Correct!";
+    answer10.style.color = "#00e030";
     prize++;
   }
   else {
     answer10.textContent = "Please try again";
+    answer10.style.color = "#f40430";
   }
 }
 times10();
@@ -142,10 +160,12 @@ var timesBy11 = document.getElementById("input11").value;
 var answer11 = document.getElementById("answer11");
   if (timesBy11 == 22) {
     answer11.textContent = "Correct!";
+    answer11.style.color = "#00e030";
     prize++;
   }
   else {
     answer11.textContent = "Please try again";
+    answer11.style.color = "#f40430";
   }
 }
 times11();
@@ -155,10 +175,12 @@ var timesBy12 = document.getElementById("input12").value;
 var answer12 = document.getElementById("answer12");
   if (timesBy12 == 24) {
     answer12.textContent = "Correct!";
+    answer12.style.color = "#00e030";
     prize++;
   }
   else {
     answer12.textContent = "Please try again";
+    answer12.style.color = "#f40430";
   }
 }
 times12();
