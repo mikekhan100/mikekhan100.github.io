4f8bd116080af0670e37646388f28dfbcac8105c
Michael Khan <mikekhan100@users.noreply.github.com>
2017-07-27 20:47:11 +0100

    Update Two Times Tables.js

--- Times Tables App/Two Times Tables.js
+++ Times Tables App/Two Times Tables.js
@@ -189,7 +189,7 @@ times12();
   if (prize === 12) {
     var result = document.getElementById("image");
     var photo = document.createElement("img");
-    photo.src = 'mikekhan100.github.io/Images/2016-12-11 14.16.10 Adjusted.jpg';
+    photo.src = './Images/2016-12-11 14.16.10 Adjusted.jpg';
     result.appendChild(photo);
     var text = document.getElementById("message");
     text.textContent = "Well done Mary!!!";
