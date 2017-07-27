4f8b23378668e1625d9de8a4bc89c081443ad164
Michael Khan <mikekhan100@users.noreply.github.com>
2017-07-27 13:53:38 +0100

    Update Four Times Tables.js

--- Times Tables App/Four Times Tables.js
+++ Times Tables App/Four Times Tables.js
@@ -191,7 +191,10 @@ times12();
     var photo = document.createElement("img");
     photo.src = 'https://www.dropbox.com/s/hwnlpfv2r3cbkt2/2017-07-22%2018.35.07.jpg?dl=1';
     result.appendChild(photo);
+    var text = document.getElementById("message");
+    text.textContent = "Well done Mary!!!";
+    text.style.color = "#f90000";
+    text.style.fontSize = "xx-large";
   }
-
   
 } // Global function closing brace
