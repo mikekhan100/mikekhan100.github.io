dc5ff1dec44c1e8982057f2471ee4a3ae78485e8
Michael Khan <mikekhan100@users.noreply.github.com>
2018-06-22 23:00:20 +0100

    Update mealPlanner.js

--- Meal Planner/mealPlanner.js
+++ Meal Planner/mealPlanner.js
@@ -1,40 +1,150 @@
-var breakfast = [
+var CandMBreakfast = [
 	'Cereal',
 	'Muesli',
 	'Spinach Omelette',
+	'Berries and Yoghurt',
+	'Banana and Yoghurt'
+];
+
+var MandBBreakfast = [
+	'Cereal',
+	'Muesli',
+	'Bacon Sandwich',
 	'Berries and Yoghurt'
 ];
 
-var lunch = [
-	'Pho!',
-	'Salad',
-	'Spinach Omelette'
+var DandMBreakfast = [
+	'Cereal'
 ];
 
-var dinner = [
+var CandMLunch = [
+	'Roast Vegetable Salad',
+	'New Potato and Anchovy Salad',
+	'Spinach Omelette',
+	'Houmous and Dips',
+	'Pho!'
+];
+
+var MandBLunch = [
+	'Chicken Sandwich',
+	'Chicken and Sweetcorn Soup',
+	'Pizza',
+	'Beans on Toast',
+	'Omelette',
+	'Pho!'
+];
+
+var DandMLunch = [
+	'Soup'
+];
+
+var CandMDinner = [
 	'Curry',
 	'Spaghetti Bolognese',
 	'Roast Dinner',
-	'BBQ'
+	'BBQ',
+	'Fish and Chips',
+	'Hot Halloween Beanpot',
+	'Vietnamese Salad',
+	'Stir Fry'
 ];
 
-var selectMenu = document.getElementById("meal");
-var btn = document.getElementById("makeChoice");
+var MandBDinner = [
+	'Curry',
+	'Spaghetti Bolognese',
+	'Roast Dinner',
+	'BBQ',
+	'Fish and Chips',
+	'Hot Halloween Beanpot',
+	'Vietnamese Salad',
+	'Stir Fry'
+];
+
+var DandMDinner = [
+	'Curry',
+	'Roast Dinner',
+	'BBQ',
+	'Fish and Chips',
+	'Hot Halloween Beanpot',
+	'Eggs, Beans and Chips',
+	'Ready Meal'
+];
+
+var mealIndex = document.getElementById("meal").selectedIndex;
+var meatIndex = document.getElementById("meat").selectedIndex;
 var result = document.getElementById("result");
+var text = '';	//used to build up the result
+var picMB = document.getElementById("picMB");
+var picCM = document.getElementById("picCM");
+var picDM = document.getElementById("picDM");
 
-btn.addEventListener('click', function() {
-	var index = selectMenu.selectedIndex;
-    
-    if (index === 0){
-		result.textContent = breakfast[Math.floor(Math.random() * (breakfast.length))];
+
+picMB.addEventListener('click', function(e) {
+	
+    if (mealIndex === 0 && meatIndex >= 0) {
+		MandBBreakfast.forEach(function(e) {
+			text += e + "\n";
+		});
+		result.textContent = text;
 	}
-
-	else if (index === 1){
-		result.textContent = lunch[Math.floor(Math.random() * (lunch.length))];
+	else if (mealIndex === 1 && meatIndex >= 0) {
+		MandBLunch.forEach(function(e) {
+			text += e + "\n";
+		});
+		result.textContent = text;
 	}
-
-	else
-		result.textContent = dinner[Math.floor(Math.random() * (dinner.length))];
+	else {
+		MandBDinner.forEach(function(e) {
+			text += e + "\n";
+		});
+		result.textContent = text;
+	}
+	e.preventDefault();
 });
 
+picCM.addEventListener('click', function(e) {
+	
+    if (mealIndex === 0 && meatIndex >= 0) {
+		CandMBreakfast.forEach(function(e) {
+			text += e + "\n";
+		});
+		result.textContent = text;
+	}
+	else if (mealIndex === 1 && meatIndex >= 0) {
+		CandMLunch.forEach(function(e) {
+			text += e + "\n";
+		});
+		result.textContent = text;
+	}
+	else {
+		CandMDinner.forEach(function(e) {
+			text += e + "\n";
+		});
+		result.textContent = text;
+	}
+	e.preventDefault();
+});
+
+picDM.addEventListener('click', function(e) {
+	
+    if (mealIndex === 0 && meatIndex >= 0) {
+		DandMBreakfast.forEach(function(e) {
+			text += e + "\n";
+		});
+		result.textContent = text;
+	}
+	else if (mealIndex === 1 && meatIndex >= 0) {
+		DandMLunch.forEach(function(e) {
+			text += e + "\n";
+		});
+		result.textContent = text;
+	}
+	else {
+		DandMDinner.forEach(function(e) {
+			text += e + "\n";
+		});
+		result.textContent = text;
+	}
+	e.preventDefault();
+});
 
